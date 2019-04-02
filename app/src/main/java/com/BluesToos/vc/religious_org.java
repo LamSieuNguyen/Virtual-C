cb139ac239bf60cd55ad017d14e58d77b13b0208
Lam Nguyen <oregon597@gmail.com>
2019-04-02 15:53:08 -0700

    commit

---
+++ app/src/main/java/com/BluesToos/vc/religious_org.java
@@ -0,0 +1,22 @@
+package com.BluesToos.vc;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class religious_org extends Fragment {
+
+    View myView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.religious_org,container,false);
+        return myView;
+    }
+}
