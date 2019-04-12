3303c9d79e376000edd4e73ed384a7602302abe2
Lam Nguyen <oregon597@gmail.com>
2019-04-11 18:33:12 -0700

    can now change fragments

---
+++ Virtual-C-master/Virtual-C-master/app/src/main/java/com/BluesToos/vc/Todo.java
@@ -0,0 +1,22 @@
+package com.BluesToos.vc;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Todo extends Fragment {
+
+    View myView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.things_to_do,container,false);
+        return myView;
+    }
+}
---
+++ Virtual-C-master/Virtual-C-master/app/src/main/java/com/BluesToos/vc/house_floor_plans.java
@@ -0,0 +1,22 @@
+package com.BluesToos.vc;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class house_floor_plans extends Fragment {
+
+    View myView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.house_floor_plan,container,false);
+        return myView;
+    }
+}
